ab37b9f8cdb09d5cca392bdbad636d635baf1cf9
RA4Z <rraz639@gmail.com>
2023-05-31 18:25:35 -0300

    Adicionando conexão de imagem com o firebase

--- src/telas/Principal/index.js
+++ src/telas/Principal/index.js
@@ -1,21 +1,32 @@
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, Image } from "react-native";
 import { useEffect, useState } from "react";
 import { Cabecalho } from "../../componentes/Cabecalho";
 import { CartaoInfo } from "../../componentes/CartaoInfo";
 import { NovoPostBotao } from "../../componentes/NovoPostBotao";
 import { pegarPostsTempoReal } from "../../servicos/firestore";
 import estilos from "./estilos";
+import {storage} from '../../config/firebase';
+import { ref, getDownloadURL } from 'firebase/storage'
 
 export default function Principal({ navigation }) {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
+        const imagemRef = ref(storage, 'img4.png')
+
+        getDownloadURL(imagemRef).then((url) => {
+            console.log(url)
+        })
         pegarPostsTempoReal(setPosts);
     },[])
 
     return (
         <View style={estilos.container}>
             <Cabecalho />
+            <Image 
+                source={{uri:'https://firebasestorage.googleapis.com/v0/b/raz-spaceapp.appspot.com/o/img4.png?alt=media&token=ba61e3ae-6456-4ed9-984a-20b6a659972d'}}
+                style={{width: 200, height: 200}}
+            />
             <ScrollView style={estilos.scroll} showsVerticalScrollIndicator={false}>
 
                 {posts?.map((item) => (
