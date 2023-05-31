e65c3edbcc9532f987609f397a5cc49730522d03
RA4Z <rraz639@gmail.com>
2023-05-31 18:54:11 -0300

    Inserindo url de imagem na criação do post

--- src/telas/Post/index.js
+++ src/telas/Post/index.js
@@ -5,8 +5,7 @@ import estilos from "./estilos";
 import { entradas } from "./entradas";
 import { alteraDados } from "../../utils/comum";
 import { IconeClicavel } from "../../componentes/IconeClicavel";
-import { storage } from "../../config/firebase";
-import { ref, uploadBytes } from 'firebase/storage';
+import { salvarImagem } from "../../servicos/storage";
 
 const imagemGalaxia = 'https://img.freepik.com/fotos-gratis/fundo-de-galaxia-espacial_53876-93121.jpg?w=900&t=st=1685568881~exp=1685569481~hmac=9375f278421b19e7ca2260c74cb7e712c7dc0e21c45615e4db6cf761ad653c13'
 
@@ -17,36 +16,27 @@ export default function Post({ navigation, route }) {
     const [post, setPost] = useState({
         titulo: item?.titulo || "",
         fonte: item?.fonte || "",
-        descricao: item?.descricao || ""
+        descricao: item?.descricao || "",
+        imagemUrl: item?.imagemUrl || null
     });
 
     async function salvar() {
         setDesabilitarEnvio(true);
+
+        const url = await salvarImagem(imagemGalaxia, 'galaxia')
+
         if (item) {
             await atualizarPost(item.id, post);
         } else {
-            await salvarPost(post);
+            await salvarPost({...post, imagemUrl:url});
         }
         
         navigation.goBack();
     }
 
-    async function salvarImagem() {
-        const downloadImagem = await fetch(imagemGalaxia)
-        const blobImagem = await downloadImagem.blob()
-        const imagemRef = ref(storage, 'posts/imagem.png')
-        uploadBytes(imagemRef, blobImagem).then(() => {
-            console.log('upload feito')
-        }).catch((error) => {
-            console.log(error)
-        })
-    }
 
     return (
         <View style={estilos.container}>
-            <Image 
-                source={{ uri:'https://firebasestorage.googleapis.com/v0/b/raz-spaceapp.appspot.com/o/posts%2Fimagem.png?alt=media&token=2cc99e28-939a-42d8-bb1d-e204aae8a35f'}}
-                style={{ width: 200, height: 200 }} />
             <View style={estilos.containerTitulo}>
                 <Text style={estilos.titulo}>{item ? "Editar post" : "Novo Post"}</Text>
                 <IconeClicavel 
@@ -79,7 +69,7 @@ export default function Post({ navigation, route }) {
                 ))}
             </ScrollView>
 
-            <TouchableOpacity style={estilos.botao} onPress={salvarImagem} disabled={desabilitarEnvio}>
+            <TouchableOpacity style={estilos.botao} onPress={salvar} disabled={desabilitarEnvio}>
                 <Text style={estilos.textoBotao}>Salvar</Text>
             </TouchableOpacity>
         </View>
